15457aaf6f6b95287bcab0b07ea847ea6c7476eb
zakymzn <mamurzakynurrokhman@gmail.com>
2025-03-25 23:30:50 +0700

    codelab melakukan pengujian menggunakan JUnit dan Mockito

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.inline)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
